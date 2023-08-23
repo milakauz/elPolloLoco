48aee99dc9d8cb000e673b8e86230b76eaa8ed54
Selina Karlin <karlinselina@gmail.com>
2023-08-23 16:28:53 +0200

    fix: typo

--- levels/level1.js
+++ levels/level1.js
@@ -44,11 +44,11 @@ const level1 = new Level (
         new Coin('img/8_coin/coin_1.png'),
     ], 
     [
-        new Bottles('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
-        new Bottles('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
-        new Bottles('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
-        new Bottles('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
-        new Bottles('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
-        new Bottles('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
+        new Bottle('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
+        new Bottle('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
+        new Bottle('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
+        new Bottle('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
+        new Bottle('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
+        new Bottle('img/6_salsa_bottle/1_salsa_bottle_on_ground.png'), 
     ]
 );
