cff3d03860af44b60a59e3e20b94e0d22b4b72e5
Selina Karlin <karlinselina@gmail.com>
2023-08-23 17:34:26 +0200

    refactor: animate() & jump()

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObject {
     width = 200;
     height = 300;
-    y = 160;
+    y = 20;
     world;
     speed = 10;
     IMAGES_WALKING = [
@@ -12,42 +12,57 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ];
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png',
+    ]
     walking_sound = new Audio('audio/running.mp3');
 
     constructor() {
-        super().loadImage('./img/2_character_pepe/2_walk/W-21.png')
-        this.loadImages(this.IMAGES_WALKING)
+        super().loadImage('./img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
 
         this.animate();
+        this.applyGravity();
     }
 
     animate() {
         setInterval(() => {
+
+
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.x += this.speed;
-                this.otherDirection = false;
                 this.walking_sound.play();
+                this.moveRight();
             }
 
             if (this.world.keyboard.LEFT && this.x > 0) {
-                this.x -= this.speed;
-                this.otherDirection = true;
                 this.walking_sound.play();
-
+                this.moveLeft();
             }
 
-            this.world.camera_x = -this.x + 100 ;
+            if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+                this.jump();
+            }
+
+            this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
         setInterval(() => {
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            }
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 // Walk animation
                 this.playAnimation(this.IMAGES_WALKING);
             }
         }, 50);
-    }
-
-    jump() {
-
     }
 }
