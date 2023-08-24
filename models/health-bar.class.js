6c30e9c62d9263ff327befe157e2ecd32bffe898
Selina Karlin <karlinselina@gmail.com>
2023-08-24 16:22:58 +0200

    new statusbars healtbar for showing energy level of character

---
+++ models/health-bar.class.js
@@ -0,0 +1,27 @@
+class HealthBar extends StatusBar {
+    percentage = 100;
+
+    IMAGES = [
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
+    ];
+
+    constructor() {
+        super();
+        // this.loadImage('img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png');
+        this.loadImages(this.IMAGES);
+        this.x = 50;
+        this.y = 50;
+        this.setPercentage(100);
+    }
+
+    setPercentage(percentage) {
+        this.percentage = percentage;
+        let imagePath = this.IMAGES[this.resolveImageIndex()];
+        this.img = this.imageCache[imagePath];
+    }
+}
---
+++ models/statusbar.class.js
@@ -0,0 +1,20 @@
+class StatusBar extends drawableObject {
+    height = 50;
+    width = 250;
+
+    resolveImageIndex() {
+        if (this.percentage == 100) {
+            return 5;
+        } else if (this.percentage > 80) {
+            return 4;
+        } else if (this.percentage > 60) {
+            return 3;
+        } else if (this.percentage > 40) {
+            return 2;
+        } else if (this.percentage > 20) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
