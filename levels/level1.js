62e906f1d3e274df4df0ee61c68633b188c563a1
Selina Karlin <karlinselina@gmail.com>
2023-08-24 13:20:45 +0200

    removing 3 chicken

--- levels/level1.js
+++ levels/level1.js
@@ -5,9 +5,6 @@ const level1 = new Level (
         new Chicken(),
         new Chicken(),
         new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
         new Endboss(), 
     ],
     [
