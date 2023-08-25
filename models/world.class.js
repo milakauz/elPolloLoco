5d8f229491e91a2d44aa0e78747d04952075562c
Selina Karlin <karlinselina@gmail.com>
2023-08-25 12:32:37 +0200

    feat: run() for several Intervals: checkCollision() checkThrowObjects implementing bottle+coin bar

--- models/world.class.js
+++ models/world.class.js
@@ -5,6 +5,9 @@ class World {
     ctx;
     camera_x = 0;
     healthBar = new HealthBar();
+    coinsBar = new CoinBar();
+    bottlesBar = new BottleBar();
+    throwableObjects = [];
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -12,7 +15,7 @@ class World {
         this.keyboard = keyboard;
         this.draw();
         this.setWorld();
-        this.checkCollisions();
+        this.run();
     }
 
     setWorld() {
@@ -20,16 +23,40 @@ class World {
         this.character.world = this;
     }
 
-    checkCollisions() {
+    run() {
         setInterval(() => {
-            this.level.enemies.forEach((enemy) => {
-                if (this.character.isColliding(enemy)) {
-                    this.character.hit();
-                    this.healthBar.setPercentage(this.character.energy)
-                }
-            });
+            this.checkCollisions();
+            this.checkThrowObjects();
         }, 200);
     }
+
+    checkThrowObjects() {
+        if (this.keyboard.D) {
+            let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100);
+            this.throwableObjects.push(bottle);
+        }
+    }
+
+    checkCollisions() {
+        this.level.enemies.forEach((enemy) => {
+            if (this.character.isColliding(enemy)) {
+                this.character.hit();
+                this.healthBar.setPercentage(this.character.energy)
+            }
+        });
+    }
+
+    //     this.level.bottles.forEach((bottle, i) => {
+    //     if (this.character.isColliding(bottle)) {
+    //         console.log('FLASCHE GETROFFEN!');
+    //         // this.character.collectedBottles.push(+1);
+    //         console.log(this.character.collectedBottles);
+    //         // Charakter muss sammeln 
+    //         // Bottle muss bei collision sofort gelöscht werden
+    //         this.ctx.clearRect(bottle.y, bottle.x, bottle.width, bottle.height);
+    //         this.level.bottles.splice(i, 1);
+    //     }
+    // };
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -37,12 +64,14 @@ class World {
         this.addObjectsToMap(this.level.backgroundObjects);
         this.ctx.translate(-this.camera_x, 0); // backwards
         this.addToMap(this.healthBar);
+        this.addToMap(this.coinsBar);
+        this.addToMap(this.bottlesBar);
         this.ctx.translate(this.camera_x, 0); //forwards
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.coins);
-        this.addObjectsToMap(this.level.bottles);
+        this.addObjectsToMap(this.throwableObjects);
         this.ctx.translate(-this.camera_x, 0);
         let self = this;
         requestAnimationFrame(function () {
@@ -60,7 +89,7 @@ class World {
         if (mo.otherDirection) {
             this.flipImage(mo);
         }
-        mo.drawFrame(this.ctx); 
+        mo.drawFrame(this.ctx);
         mo.draw(this.ctx);
         if (mo.otherDirection) {
             this.flipImageBack(mo);
