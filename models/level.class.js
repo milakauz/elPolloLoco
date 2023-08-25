5cc69c55d2500bcbf0f9a6733f97efbfdf8d61ae
Selina Karlin <karlinselina@gmail.com>
2023-08-25 12:35:38 +0200

    removing bottles from level

--- models/level.class.js
+++ models/level.class.js
@@ -3,14 +3,12 @@ class Level {
     clouds;
     backgroundObjects;
     coins;
-    bottles;
     level_end_x = 2200;
 
-    constructor(enemies, clouds, backgroundObjects, coins, bottles) {
+    constructor(enemies, clouds, backgroundObjects, coins) {
         this.enemies = enemies;
         this.clouds = clouds;
         this.backgroundObjects = backgroundObjects;
         this.coins = coins;
-        this.bottles = bottles;
     }
 }
