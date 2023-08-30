f6277ec4913ca52cec27e5b1d36a1730be7fade0
Selina Karlin <karlinselina@gmail.com>
2023-08-30 11:05:34 +0200

    feat: playAnimation for rotating bottles in air after key_D

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -2,7 +2,7 @@ class ThrowableObject extends MovableObject {
     speedY = 30;
     speedX = 20;
 
-    IMAGES_THROWING = [
+    IMAGES_ROTATING = [
         'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
         'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
         'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
@@ -10,18 +10,27 @@ class ThrowableObject extends MovableObject {
     ];
 
     constructor(x, y) {
-        super().loadImage('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
+        super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.IMAGES_ROTATING);
         this.x = x;
         this.y = y;
         this.height = 60;
         this.width = 50;
         this.throw();
+        this.animate();
     }
 
     throw() {
         this.applyGravity();
         setInterval(() => {
+            this.animate();
             this.x += 10;
         }, 25);
+    }
+
+    animate() {
+        console.log('Hi, animate() wird aufgerufen');
+        //von wo?
+        this.playAnimation(this.IMAGES_ROTATING);
     }
 }
