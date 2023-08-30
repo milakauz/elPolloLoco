72921232936b60b90ba0a6d45aebd16832cb74ac
Selina Karlin <karlinselina@gmail.com>
2023-08-30 11:41:19 +0200

    feat: disappea() when colliding with character

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -18,7 +18,6 @@ class Chicken extends MovableObject {
     constructor() {
         super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
         this.loadImages(this.IMAGES_WALKING);
-        // this.loadImage(this.IMAGE_DEAD);
 
         this.x = 250 + Math.random() * 500;
         this.speed = 0.15 + Math.random() * 0.25;
@@ -36,8 +35,12 @@ class Chicken extends MovableObject {
         }, 200);
     }
 
-    dies() {
-            console.log('enemy.dies() wird aufgerufen');
-            this.loadImage(this.IMAGE_DEAD);
-    } 
+    disappear() {
+        let deathInterval = setInterval(() => {
+            this.playAnimation(this.IMAGE_DEAD);
+            setTimeout(() => {
+                clearInterval(deathInterval);
+            }, 200);
+        }, 1500);
+    }
 }
