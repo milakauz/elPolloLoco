ebedba3a30a15d532d4df33c84aa5611c231cc1f
Selina Karlin <karlinselina@gmail.com>
2023-08-31 10:11:19 +0200

    new: start_x for chicken()

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -22,7 +22,7 @@ class Chicken extends MovableObject {
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGE_DEAD);
 
-        this.x = 250 + Math.random() * 500;
+        this.x = 350 + Math.random() * 500;
         this.speed = 0.15 + Math.random() * 0.25;
 
         this.animate()
