5aee249d5f295d4b6d9f780b03dd962ea4867edc
Selina Karlin <karlinselina@gmail.com>
2023-09-04 11:33:27 +0200

    adding images for walking and being hurt trying to fix endboss loosing to much energy when being hit

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -3,7 +3,7 @@ class Endboss extends MovableObject {
     width = 400;
     y = 65;
     isDead = false;
-    isHit = false;
+    // isHit = false;
     energy = 100;
     offSet = {
         top: 20,
@@ -13,6 +13,12 @@ class Endboss extends MovableObject {
     }
     hitting_sound = new Audio('audio/hit_endboss.mp3');
     IMAGES_WALKING = [
+        'img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img/4_enemie_boss_chicken/1_walk/G4.png',
+    ]
+    IMAGES_ALERTING = [
         'img/4_enemie_boss_chicken/2_alert/G5.png',
         'img/4_enemie_boss_chicken/2_alert/G6.png',
         'img/4_enemie_boss_chicken/2_alert/G7.png',
@@ -32,41 +38,30 @@ class Endboss extends MovableObject {
     ]
 
     constructor() {
-        super().loadImage(this.IMAGES_WALKING[0]);
-        this.loadImages(this.IMAGES_WALKING);
+        super().loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_HURTING);
+        this.loadImages(this.IMAGES_ALERTING);
 
-        this.x = 2200;
-        // this.animate();
+        this.x = 2400;
+        this.animate();
     }
 
     animate() {
-        // this.moveLeft();
-
-        let hurtingInterval = setInterval(() => {
-            if (this.isHit) {
-                this.playAnimation(this.IMAGES_HURTING);
-                console.log('bilder sollen gespieltwerden');
-                setTimeout(() => {
-                    clearInterval(hurtingInterval);
-                }, 300);
-            }
-        }, 150);
+        let movingEndboss = setInterval(() => {
+            this.moveLeft()
+        }, 1000 / 100);
 
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
+            if (!this.isHurt()) {
+                this.playAnimation(this.IMAGES_WALKING)
+            } else if (this.isHurt()) {
+                setTimeout(() => {
+                    this.playAnimation(this.IMAGES_HURTING);
+                }, 500);
+            }
         }, 200);
     }
 
-    isHitFromBottle() {
-        this.isHit = true
-        if (this.isHit) {
-            setTimeout(() => {
-                this.isHit = false;
-            }, 200);
-        }
-        console.log(this.isHit);
-    }
 
     checkEndbossEnergy() {
         console.log(this.energy);
