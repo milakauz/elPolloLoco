edfb3ae60c132485a44471ad0fa7f2ed2cae2f0c
Selina Karlin <karlinselina@gmail.com>
2023-09-04 17:18:53 +0200

    cleanse comments

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -26,20 +26,23 @@ class Endboss extends MovableObject {
         'img/4_enemie_boss_chicken/2_alert/G10.png',
         'img/4_enemie_boss_chicken/2_alert/G11.png',
         'img/4_enemie_boss_chicken/2_alert/G12.png',
-        // 'img/4_enemie_boss_chicken/1_walk/G2.png',
-        // 'img/4_enemie_boss_chicken/1_walk/G3.png',
-        // 'img/4_enemie_boss_chicken/1_walk/G4.png'
     ]
     IMAGES_HURTING = [
         'img/4_enemie_boss_chicken/4_hurt/G21.png',
         'img/4_enemie_boss_chicken/4_hurt/G22.png',
         'img/4_enemie_boss_chicken/4_hurt/G23.png',
     ]
+    IMAGES_DYING = [
+        'img/4_enemie_boss_chicken/5_dead/G24.png',
+        'img/4_enemie_boss_chicken/5_dead/G25.png',
+        'img/4_enemie_boss_chicken/5_dead/G26.png'
+    ]
 
     constructor() {
         super().loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_HURTING);
         this.loadImages(this.IMAGES_ALERTING);
+        this.loadImages(this.IMAGES_DYING);
 
         this.x = 2400;
         this.animate();
