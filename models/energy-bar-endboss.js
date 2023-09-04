d2f327891874970afb51fccc3ed380931d4bce38
Selina Karlin <karlinselina@gmail.com>
2023-09-04 17:19:10 +0200

    adding energy bar to endboss

---
+++ models/energy-bar-endboss.js
@@ -0,0 +1,20 @@
+class EnergyBar extends StatusBar {
+    x = 500;
+    percentage = 100;
+
+    IMAGES = [
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png',
+    ];
+
+    constructor() {
+        super();
+        this.loadImages(this.IMAGES);
+        this.y = 50;
+        this.setPercentage(100);
+    }
+}
