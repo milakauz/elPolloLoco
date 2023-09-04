901a6d40348dd22ef93caca2aaf3352f6c75f74f
Selina Karlin <karlinselina@gmail.com>
2023-09-04 17:20:12 +0200

    adding images for splashing bottles

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -8,6 +8,7 @@ class ThrowableObject extends MovableObject {
         bottom: 0,
         left: 0
     }
+    isSplashed = false;
 
     IMAGES_ROTATING = [
         'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
@@ -16,9 +17,17 @@ class ThrowableObject extends MovableObject {
         'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
     ];
 
+    IMAGES_SPLASHING = [
+        './img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        './img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        './img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        './img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+    ]
+
     constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
         this.loadImages(this.IMAGES_ROTATING);
+        this.loadImages(this.IMAGES_SPLASHING);
         this.x = x;
         this.y = y;
         this.height = 60;
@@ -37,7 +46,11 @@ class ThrowableObject extends MovableObject {
 
     animate() {
         setInterval(() => {
-            this.playAnimation(this.IMAGES_ROTATING);
+            if (!this.isSplashed) {
+                this.playAnimation(this.IMAGES_ROTATING);
+            } else if (this.isSplashed) {
+                this.playAnimation(this.IMAGES_SPLASHING)
+            }
         }, 150);
     }
 }
