c820fa350ccf63503ef60efbfd32c4afc655e19b
Selina Karlin <karlinselina@gmail.com>
2023-09-05 09:32:52 +0200

    mute() unmute()

--- js/sounds.js
+++ js/sounds.js
@@ -9,12 +9,12 @@ const bottle_collecting_sound = new Audio('audio/collect_bottle.mp3');
 const bottle_splashing = new Audio('./audio/glass.mp3')
 const coin_collecting_sound = new Audio('audio/collect_coin.mp3');
 
-var muted = false;
+// var muted = false;
 
 let sounds = [
     character_jumping_sound,
     character_walking_sound,
-    endboss_hitting_sound, 
+    endboss_hitting_sound,
     bottle_collecting_sound,
     coin_collecting_sound
 ]
@@ -22,21 +22,25 @@ let sounds = [
 function mute() {
     if (muted == false) {
         muted = true;
-        // soundOffContainer.classList.add('d-none');
-        // soundOnContainer.classList.remove('d-none');
         for (let i = 0; i < sounds.length; i++) {
             const sound = sounds[i];
-            sound.muted();
+            sound.muted = true;
         }
         console.log('LEISE!')
+        soundOffContainer.classList.add('d-none');
+        soundOnContainer.classList.remove('d-none');
     }
 }
 
 function unmute() {
     if (muted == true) {
         muted = false;
-        // soundOnContainer.classList.add('d-none');
-        // soundOffContainer.classList.remove('d-none');
-
+        soundOnContainer.classList.add('d-none');
+        soundOffContainer.classList.remove('d-none');
+        for (let i = 0; i < sounds.length; i++) {
+            const sound = sounds[i];
+            sound.muted = false;
+        }
+        console.log('LAUT!');
     }
 }
